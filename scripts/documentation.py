39d87a107ec8b667ee965f4752115c96fd3ec282
jochem725 <jochemlugtenburg@gmail.com>
2021-03-01 18:31:18 +0100

    Generate leaderboard data

--- scripts/documentation.py
+++ scripts/documentation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from tabulate import tabulate
+from datetime import datetime
 
 def generate_track_table(track_data):
     headers = ['Image', 'Name', 'Arn Name', 'Description']
@@ -34,7 +35,45 @@ Currently there are **{} tracks** available in the dataset.
 
     generate_track_table(track_data)
 
+def generate_leaderboards_table(leaderboards_data):
+    headers = ['Name', 'Data', 'Status', 'Launch Time (UTC)', 'Close Time (UTC)', '# Participants', 'Winner']
+    table = []
+    for index, row in leaderboards_data.iterrows():
+        tablerow = [
+            '**{}**'.format(row['Name']),
+            'TODO LINK',
+            row['Status'],
+            '*{}*'.format(datetime.utcfromtimestamp(int(row['LaunchTime'] / 1000))),
+            '*{}*'.format(datetime.utcfromtimestamp(int(row['CloseTime'] / 1000))),
+            row['ParticipantCount'],
+            row['WinnerAlias']
+        ]
         
+        table.append(tablerow)
+
+    return tabulate(table, headers, tablefmt="github")
+
+
+        
+def update_leaderboards_documentation():
+    leaderboards_data = pd.read_csv('./raw_data/leaderboards/leaderboards.csv')
+    leaderboards_data = leaderboards_data.sort_values('CloseTime', ascending=False).fillna('')
+
+    readme = \
+    """
+# Leaderboards
+## Leaderboard update interval
+Track data is updated at the first minute of each hour. Actual update time may vary slightly due to the way actions are scheduled.
+## Available tracks
+Currently there are **{} leaderboards** available in the dataset.
+{}
+    """.format(len(leaderboards_data), generate_leaderboards_table(leaderboards_data))
+
+    with open('./raw_data/leaderboards/README.md', 'w') as f:
+        f.write(readme)
+
+    generate_leaderboards_table(leaderboards_data)
 
 if __name__ == "__main__":
     update_track_documentation()
+    update_leaderboards_documentation()
