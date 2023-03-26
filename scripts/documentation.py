5cf55cc09a113bf13489fdd724934607ad5c5df9
Jochem Lugtenburg <jochem725@users.noreply.github.com>
2023-03-26 15:23:58 +0200

    Fetch latest data from ECR instead of S3 (#28)
    * Update documentation script to handle CW/CCW
    
    * Bump stats version

--- scripts/documentation.py
+++ scripts/documentation.py
@@ -4,21 +4,26 @@ from datetime import datetime
 from urllib.parse import urlparse
 import glob
 import os
+import re
 
 def format_utc_timestamp(ts_in_ms):
     return datetime.utcfromtimestamp(int(ts_in_ms/ 1000)).strftime('%Y-%m-%dT%H:%M:%SZ')
 
 def generate_track_table(track_data):
-    headers = ['Image', 'Name', 'Release Date', 'Numpy File', 'Track Length', 'Track Width']
+    headers = ['Image', 'Name', 'Release Date', 'Numpy Files', 'Track Length', 'Track Width']
     table = []
 
     def format_npy(arn):
         # Find last file we have.
-        files = [os.path.basename(f) for f in glob.glob('./raw_data/tracks/npy/*.npy')]
-        npy_file = "{}.npy".format(arn.split("/")[-1])
+        track_name = arn.split("/")[-1]
+        files = [os.path.basename(f) for f in glob.glob(f'./raw_data/tracks/npy/{track_name}*.npy')]
+        accepted_files = [f"{track_name}.npy", f"{track_name}_cw.npy", f"{track_name}_ccw.npy"]
 
-        if npy_file in files:
-            return '[{}](./{})'.format(npy_file, os.path.join("./npy", npy_file))
+        files = list(set(files).intersection(accepted_files))
+
+        if len(files) > 0:
+            formatted_files = map(lambda f: '- [{}](./{})'.format(f, os.path.join("./npy", f)), files)
+            return " ".join(files)
         else:
             return "-"
 
